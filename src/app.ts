3d9f4fe04b05eef70f730d744f4fb49248971acc
Felipe Botero <59348629+FeBotero@users.noreply.github.com>
2023-04-15 14:16:35 -0400

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,22 @@
 import express from "express";
-import { Request, Response, Router } from "express";
 import cors from "cors";
+import connection from "./config/database";
+
+import userRouter from "./routes/user.route";
+import carRouter from "./routes/car.route";
 
 const app = express();
-app.use(cors());
 const port = 3000;
 
-app.listen(port, () => console.log("Server funcionando"));
+app.use(cors());
+app.use(express.json());
+
+app.use("/user", userRouter);
+app.use("/car", carRouter);
+
+connection
+  .then(() => {
+    console.log("Banco de dados Conectado!");
+    app.listen(port, () => console.log("Server funcionando"));
+  })
+  .catch((error) => console.log(error));

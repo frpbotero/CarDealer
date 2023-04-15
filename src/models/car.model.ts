b596156a23f7cb2c4f3fe4b88c2cda6da2d2cb0c
Felipe Botero <59348629+FeBotero@users.noreply.github.com>
2023-04-14 23:33:07 -0400

    Create car.model.ts

---
+++ src/models/car.model.ts
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+
+interface ICar {
+  plate: string;
+  model: string;
+  year: number;
+  brand: string;
+  color: string;
+  buyValue: number;
+}
+export const CarSchema = new Schema({
+  plate: { type: String, require: true },
+  model: { type: String, require: true },
+  year: { type: Number, require: true },
+  brand: { type: String, require: true },
+  color: { type: String, require: true },
+  buyValue: { type: Number, require: true },
+});
+
+export const Car = mongoose.model<ICar>("Car", CarSchema);

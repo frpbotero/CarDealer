28c25bb8ade01ef78202dda014fecbd3a7ea514a
Felipe Botero <59348629+FeBotero@users.noreply.github.com>
2023-05-04 11:28:19 -0400

    Update car.service.ts

--- src/services/car.service.ts
+++ src/services/car.service.ts
@@ -1,5 +1,6 @@
 import CarRepository from "../repositories/car.repository";
 import { ICar } from "../models/car.model";
+import { validateCreateCar, validateUpdateCar } from "../utils/cars.schema";
 
 class CarService {
   getAll() {
@@ -9,6 +10,8 @@ class CarService {
     return CarRepository.getByID(id);
   }
   async create(body: ICar) {
+    const { error, value } = validateCreateCar(body);
+    if (error) throw new Error("Favor verificar os dados enviados!");
     const car = await CarRepository.getByPlate(body.plate);
     if (car) throw new Error("Carro já cadastrado!");
     return CarRepository.create(body);
@@ -17,6 +20,8 @@ class CarService {
     return CarRepository.deleteById(id);
   }
   update(id: string, body: Partial<ICar>) {
+    const { error, value } = validateUpdateCar(body);
+    if (error) throw new Error("Favor verificar os dados enviados!");
     return CarRepository.updateById(id, body);
   }
 }

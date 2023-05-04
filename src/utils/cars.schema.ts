d33949db3f203bfeee14653da3186610357c059b
Felipe Botero <59348629+FeBotero@users.noreply.github.com>
2023-05-04 11:24:53 -0400

    create Car.schema

---
+++ src/utils/cars.schema.ts
@@ -0,0 +1,29 @@
+import Joi from "joi";
+import { ICar } from "../models/car.model";
+
+const validator =
+  (schema: Joi.ObjectSchema<ICar>) => (requestBody: Partial<ICar>) =>
+    schema.validate(requestBody);
+
+const createCarSchema = Joi.object({
+  plate: Joi.string()
+    .min(7)
+    .max(7)
+    .required()
+    .message("Favor informar apenas os caracteres"),
+  model: Joi.string().required(),
+  year: Joi.number().min(4).required(),
+  brand: Joi.string().min(3).required(),
+  color: Joi.string().required(),
+  buyValue: Joi.number().required(),
+});
+const updateCarSchema = Joi.object({
+  plate: Joi.forbidden(),
+  model: Joi.string(),
+  year: Joi.number().min(4),
+  brand: Joi.string().min(3),
+  color: Joi.string(),
+  buyValue: Joi.number(),
+});
+export const validateCreateCar = validator(createCarSchema);
+export const validateUpdateCar = validator(updateCarSchema);
